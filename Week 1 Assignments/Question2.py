8ffb31000845cfed2b8ec7f9c7c8263c061f7896
Infinity <45239108+Akhila-Krishna@users.noreply.github.com>
2020-10-11 00:58:34 +0530

    Delete Question2.py

--- Week 1 Assignments/Question2.py
+++
@@ -1,4 +0,0 @@
-#Write a program that takes an amount in Indian Rupees as input. You need to find its equivalent in Euro and display it. Assume 1 Rupee equals 80 Euros.  
-
-a=int(input())
-print(a*80)

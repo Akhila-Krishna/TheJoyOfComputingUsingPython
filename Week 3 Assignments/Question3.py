2389779de1a2b3859fdcd647a7d6d354c18f8908
Infinity <45239108+Akhila-Krishna@users.noreply.github.com>
2020-10-11 00:51:05 +0530

    Update and rename Question4.py to Question3.py

--- Week 3 Assignments/Question3.py
+++ Week 3 Assignments/Question3.py
@@ -1,18 +1,16 @@
 # In this program, create a list of numbers from 1 to 50 named list_1. The numbers should be present in the increasing order: Ex list_1 = [1,2,3,4,5,....,50]
 # i.e. index zero should be 1, index one should be 2, index two should be 3 and so on.
-# Given an input of two numbers, let's say a and b, you have to print the numbers returned by the following command
-# list_1[a:b] 
+# Given an input let's say a, you have to print the number of elements of list_1 which are divisible by a,  excluding the element which is equal to a.
 
 l = []
 for i in range(1,51):
     l.append(i)
 
-a, b = input().split()
-
+a= int(input())
 a = int(a)
-b = int(b)
 
-x = l[a:b]
-
-for i in x:
-    print(i)
+count = 0
+for i in l:
+    if(i%a==0 and i!=a):
+        count+=1
+print(count)

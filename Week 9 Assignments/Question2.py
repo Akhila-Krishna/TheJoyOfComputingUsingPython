682ab977fd20032c3dbfbad32d6cd86786cb69b4
Infinity <45239108+Akhila-Krishna@users.noreply.github.com>
2020-11-09 12:44:42 +0530

    Update Question2.py

--- Week 9 Assignments/Question2.py
+++ Week 9 Assignments/Question2.py
@@ -1,9 +1,9 @@
-// Given a string S having characters from English alphabets ['a' - 'z'] and '.' as the special character (without quotes). 
-// Write a program to construct the lexicographically smallest palindrome by filling each of the faded character ('.') with a lower case alphabet.
+# Given a string S having characters from English alphabets ['a' - 'z'] and '.' as the special character (without quotes). 
+# Write a program to construct the lexicographically smallest palindrome by filling each of the faded character ('.') with a lower case alphabet.
 
-// Definition:
-// The smallest lexicographical order is an order relation where string s is smaller than t, given the first character of s (s1 ) is smaller than the first character 
-// of t (t1 ), or in case they are equivalent, the second character, etc.
+# Definition:
+# The smallest lexicographical order is an order relation where string s is smaller than t, given the first character of s (s1 ) is smaller than the first character 
+# of t (t1 ), or in case they are equivalent, the second character, etc.
 
 def constructPalin(string, l): 
 	string = list(string) 

193d8e5e084a9006488fd398909f290052579635
Infinity <45239108+Akhila-Krishna@users.noreply.github.com>
2020-11-09 12:44:03 +0530

    Update Question1.py

--- Week 9 Assignments/Question1.py
+++ Week 9 Assignments/Question1.py
@@ -1,4 +1,4 @@
-// Given a string, write a program to check if it is palindrome or not. A string is said to be a palindrome if the reverse of the string is the same as string.
+# Given a string, write a program to check if it is palindrome or not. A string is said to be a palindrome if the reverse of the string is the same as string.
 
 def isPalindrome(s):
 	return s == s[::-1]

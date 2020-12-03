25fb97ac5c3017b0a965953da4da1323a97ecb57
Infinity <45239108+Akhila-Krishna@users.noreply.github.com>
2020-12-03 13:12:39 +0530

    Create Question1.py

---
+++ Week 12 Assignments/Question1.py
@@ -0,0 +1,6 @@
+# Write a program that accepts sequence of lines as input and prints the lines after making all characters in the sentence capitalised.
+
+n = int(input())
+for i in range(n):
+	s = input()
+	print(s.upper())
